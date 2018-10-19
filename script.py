9c1c4268f277f62f3fbe102ac54e51aea44b6f8b
Modaf <valentin.pasquale@orange.fr>
2018-10-19 03:27:19 +0200

    Update script.py

--- script.py
+++ script.py
@@ -3,8 +3,9 @@ import sys
 import discord
 import asyncio
 import datetime
+import time
 TOKEN = 'NTAyNTUzNzA1MDY1ODczNDA4.DqpnPA.f3DCUouC_-qt4B-zb-rC7nq1q5A'
-IDSALON = 502554506081599490
+IDSALON = 502560765341859841
 
 client = discord.Client()
 channel = discord.Object(id=IDSALON)
@@ -18,9 +19,16 @@ def somme(liste) :
     for k in liste :
         res += k+" "
     return res[:-1]
+async def objectif_atteint(temps, objectif):
+    # Sleep for one hour
+    await asyncio.sleep(temps)
+    await client.send_message(channel, "Alors ? L'objectif **"+objectif+"** a t'il été atteint ? :innocent: si oui, et :no_mouth: si non")
+    get_reaction_users
+
 
 @client.event
 async def on_message(message):
+    print(message.content)
     msg = message.content.split()
     if msg[0] == "!remindme" or msg[0]+msg[1] == "!remindme" :
         objectif = ""
@@ -30,21 +38,31 @@ async def on_message(message):
             objectif = somme(msg[1:-1])
         temps = msg[-1]
         t = datetime.datetime.now() #En heures
+        delai = 0
         if temps[-1].upper() == "H" :
             t+=datetime.timedelta(hours = int(temps[:-1]))
+            delai = int(temps[:-1])*3600
         if temps[-1].upper() == "D" :
             t+=datetime.timedelta(days = int(temps[:-1]))
+            delai = int(temps[:-1])*3600*24
         t = str(t.day)+"/"+str(t.month)+" à "+str(t.hour)+"H"
-        await client.send_message(channel, "Objectif '**"+objectif+"**' à faire avant le "+t)
+        await client.send_message(message.channel, "C'est noté ! Objectif '**"+objectif+"**' à faire avant le "+t)
+        
+        #Cette partie est executée une fois l'objectif atteint ou non
+        await asyncio.sleep(delai)
+        await client.send_message(channel, "Alors " + message.author.mention + " ? L'objectif **"+objectif+"** a t'il été atteint ? :innocent:")        
+        def check(msg):
+            return "OUI" in msg.content.upper() or "YEP" in msg.content.upper()or "NON" in msg.content.upper()
+        message = await client.wait_for_message(author=message.author, check=check)
+        await client.send_message(channel, "Reçu")
+        if "OUI" in message.content.upper() or "YEP" in message.content.upper() :
+            await client.send_message(channel, "Nice :)")
+        if "NON" in message.content.upper() :
+            await client.send_message(channel, "Deg :(")
+            
 
-    
 @client.event
 async def on_ready():
     await client.send_message(channel, "Je suis la")
 
 client.run(TOKEN)
-
-
-
-
-
